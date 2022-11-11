e5e2bb91f58d196e8004ea90ce7622fd513b3a91
DanMir0 <95764144+DanMir0@users.noreply.github.com>
2022-11-11 19:56:06 +0300

    product js

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,56 @@ import Slider from './slider'
 
 const slider = new Slider(document.querySelector('.wrapper'))
 const select = document.querySelector("#selectProduct")
+const productsItems = document.querySelectorAll('.product__menu-item')
+const allItems = document.querySelectorAll('.product__item')
+
+
+select.addEventListener('change', (e) => {
+  allItems.forEach(item => {
+    if (select.value === item.id) {
+        item.classList.remove('d-none')
+    } else {
+        item.classList.add('d-none')
+    }
+  })
+})
+
+productsItems.forEach(product => {
+  product.addEventListener('click', function (event) {
+    let a = document.querySelector('.product__menu-item-active')
+    a.classList.remove('product__menu-item-active')
+    const productsItemsSwipe = document.querySelectorAll('.product__item-group')
+    productsItemsSwipe.forEach(el => {
+      el.classList.add('d-none')
+    })
+ 
+
+    let info = document.querySelector(`.${this.id}`)
+    info.classList.remove('d-none')
+
+    product.classList.add('product__menu-item-active')
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // select.addEventListener('change', () => {
 //     if (select.value === "select_one") {
@@ -19,19 +69,3 @@ const select = document.querySelector("#selectProduct")
 //         select.style.backgroundImage = "url('./img/select_six.svg')"
 //     } 
 // })  
-
-const allItems = document.querySelectorAll('.product__item')
-
-
-
-select.addEventListener('change', (e) => {
-  allItems.forEach(item => {
-    if (select.value === item.id) {
-        item.classList.remove('d-none')
-    } else {
-        item.classList.add('d-none')
-    }
-  })
-})
-
-
